266000f589f354dec5cea6b144307be2984571ab
Roman Hudec <roman@ksp.sk>
2014-04-02 22:51:26 +0200

    fixes for syslinux 6

--- prepare_boot.sh
+++ prepare_boot.sh
@@ -5,14 +5,15 @@ WORK_DIR="$(mktemp -d)"
 cd "$(dirname "$0")"
 
 KERNEL_VER=aufs_friendly
-SYSLINUX_MODULES="chain.c32 cmd.c32 isolinux.bin reboot.c32 vesamenu.c32 whichsys.c32"
+BOOT_MODE=bios
+SYSLINUX_MODULES="chain.c32 cmd.c32 isolinux.bin reboot.c32 vesamenu.c32 whichsys.c32 ldlinux.c32 libcom32.c32 libutil.c32"
 
 cp -a boot "$WORK_DIR/boot"
 cp -a /boot/memtest86+/memtest.bin "$WORK_DIR/boot/"
 for MOD in $SYSLINUX_MODULES; do
-    cp -a "/usr/lib/syslinux/$MOD" "$WORK_DIR/boot/syslinux/"
+    cp -a "/usr/lib/syslinux/$BOOT_MODE/$MOD" "$WORK_DIR/boot/syslinux/"
 done
-cp -a "/usr/lib/syslinux/pxelinux.0" "$WORK_DIR/boot/"
+cp -a "/usr/lib/syslinux/$BOOT_MODE/pxelinux.0" "$WORK_DIR/boot/"
 cp -a "/boot/vmlinuz-linux-$KERNEL_VER" "$WORK_DIR/boot/vmlinuz"
 cp -a "/boot/oi_boot-$KERNEL_VER.img" "$WORK_DIR/boot/oi_boot.img"
 
